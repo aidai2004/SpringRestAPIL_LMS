56609dcf5ef90cfa73b58aa542af1cd304f3c697
aidai <izumisoft1@mail.ru>
2023-03-01 00:21:13 +0900

    Initial commit

---
+++ src/main/java/com/peaksoft/dto/CourseRequest.java
@@ -0,0 +1,17 @@
+package com.peaksoft.dto;
+
+import com.peaksoft.entity.Company;
+import com.peaksoft.entity.Group;
+import com.peaksoft.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+@Getter@Setter
+public class CourseRequest {
+    private String courseName;
+    private String durationMonth;
+    private Long companyId;
+    private List<Group> groups;
+}
